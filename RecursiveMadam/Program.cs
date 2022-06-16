4b23d5304f62ba04aee0c997a80c6c014d9c13eb
Amir Allahyari <alahyari.amir1994@gmail.com>
2022-06-16 19:20:19 +0430

    recursive string

---
+++ RecursiveMadam/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kalame morede nazar ra vared konid");
+            string word = Console.ReadLine();
+            Console.WriteLine(ReverseMyWord(word, "", word.Length));
+            
+            Console.ReadKey();
+        }
+            
+        static string ReverseMyWord(string word, string reversedWord, int i)
+        {
+            
+            if (word.Length %2 ==1 && i > word.Length / 2 + 1)
+            {
+                i--;
+                reversedWord = reversedWord + word[i];
+                return ReverseMyWord(word, reversedWord, i);
+            }
+            else if (word.Length % 2 == 0 && i > word.Length / 2)
+            {
+                i--;
+                reversedWord = reversedWord + word[i];
+                return ReverseMyWord(word, reversedWord, i);
+            }
+            else
+            {
+                if (reversedWord == word.Substring(0, reversedWord.Length))
+                    return "true";
+                else
+                    return "false";
+            };
+        }
+    }
+}
