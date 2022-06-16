3258f681bb600aa0c1521db5b6ed39b69982d3b5
Amir Allahyari <alahyari.amir1994@gmail.com>
2022-06-16 09:39:18 +0430

    tamrin revers

---
+++ revers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace reverses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kalame morede nazar ra vared konid");
+            string word = Console.ReadLine();
+            string reversedWord ="";
+
+            //for (int i = word.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(word[i]);
+            //}
+            
+            reversedWord = ReverseMyWord(word, reversedWord, word.Length);
+            Console.WriteLine(reversedWord);
+            
+            Console.ReadKey();
+        }
+            
+        static string ReverseMyWord(string word, string reversedWord, int i)
+        {
+            if (i > 0)
+            {
+                i--;
+                reversedWord = reversedWord+ word[i];
+                return ReverseMyWord(word,  reversedWord, i);
+            }
+            else
+            {
+                return reversedWord;
+            };
+        }
+    }
+}
