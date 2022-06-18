ce83f9891e6d9f817be77d5bfc3ed6cfe84745fd
Amir Allahyari <alahyari.amir1994@gmail.com>
2022-06-19 00:59:17 +0430

    tamrin lambda odd numbers

---
+++ LambdaOddNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            List<int> lstnumbers = new List<int>();
+            while (i < numbers)
+            {
+                i++;
+                lstnumbers.Add(i);
+            }
+            Console.Write("odd numbers :");
+
+            List<int> oddNumbers = lstnumbers.FindAll(a => a % 2 != 0);
+            oddNumbers.ForEach(a => Console.Write(a+" ")); 
+        }
+    }
+}
