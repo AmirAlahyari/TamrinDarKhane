fbcea49913bb9d0f2efe3447d6bfef74257e0839
Amir Allahyari <alahyari.amir1994@gmail.com>
2022-06-20 23:26:52 +0430

    odd number in lamda

---
+++ LambdaOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaSumOddNumbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int EnterNumbers = Convert.ToInt32(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+            numbers.AddRange(Enumerable.Range(0, EnterNumbers));
+
+            List<int> oddNumbers = numbers.FindAll(a => a % 2 != 0);
+
+            oddNumbers.ForEach(a => Console.Write(a + " "));
+
+        }
+    }
+}
