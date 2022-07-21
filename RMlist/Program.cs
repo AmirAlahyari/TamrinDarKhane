3448405a5dee9dd1a17ccae09c546395ae81166f
Amir Allahyari <alahyari.amir1994@gmail.com>
2022-07-21 20:27:52 +0430

    remove in list

---
+++ RMlist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RMlist
+{
+    class Program
+    {
+static void Main(string[] args)
+        {
+            List<string> myList = new List<string> {
+                                                       "Parviz",
+                                                       "Iria",
+                                                       "Peter",
+                                                       "Asghar",
+                                                       "Pangoilerv",
+                                                       "Arin",
+                                                       "Asghr"
+                                                   };
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("***************");
+
+           
+            myList.RemoveAll(m => m.StartsWith("Pa"));
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
